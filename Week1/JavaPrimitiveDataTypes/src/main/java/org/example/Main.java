93703a4d2f68d6b168de44b5ea49a39930302fcc
Jonathan Hop <yithril@gmail.com>
2023-10-04 09:28:32 -0400

    primitive data types example

---
+++ Week1/JavaPrimitiveDataTypes/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        byte myByte = 10;
+        short myShort = 1000;
+        int myInt = 42;
+        long myLong = 1234567890L;
+        float myFloat = 3.14f;
+        double myDouble = 2.71828;
+        char myChar = 'A';
+        boolean myBoolean = true;
+
+        // You can print the values to the console to see the results
+        System.out.println("myByte: " + myByte);
+        System.out.println("myShort: " + myShort);
+        System.out.println("myInt: " + myInt);
+        System.out.println("myLong: " + myLong);
+        System.out.println("myFloat: " + myFloat);
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myChar: " + myChar);
+        System.out.println("myBoolean: " + myBoolean);
+    }
+}
